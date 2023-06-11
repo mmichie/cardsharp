c3af1dd381b96ff3a09b7fb0b2e8e6c95e23d60f
Matt Michie <mmichie@gmail.com>
2023-06-11 14:22:49 -0700

    Add more sophisticated game state tracking for later analysis on high card

---
+++ cardsharp/common/state.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+class GameState(ABC):
+    """
+    An abstract base class representing the state of a game.
+    """
+
+    @abstractmethod
+    def get_state(self):
+        """
+        Return a dictionary representing the current state of the game.
+        """
+        pass
+
+    @abstractmethod
+    def update_state(self, new_state):
+        """
+        Update the game state with new information.
+        """
+        pass
+
+    @abstractmethod
+    def display_stats(self):
+        """
+        Print out the game statistics.
+        """
+        pass
--- cardsharp/high_card/high_card.py
+++ cardsharp/high_card/high_card.py
@@ -1,47 +1,64 @@
-import abc
 import argparse
 import asyncio
 import random
+import time
 
 from cardsharp.common.actor import SimplePlayer
 from cardsharp.common.deck import Deck
 from cardsharp.common.io_interface import DummyIOInterface, IOInterface
+from cardsharp.common.state import GameState
 
 
-class GameState(abc.ABC):
-    @abc.abstractmethod
+class HighCardGameState(GameState):
+    def __init__(self, player_names):
+        self.rounds_played = 0
+        self.wins = {name: 0 for name in player_names}
+        self.current_streak = {name: 0 for name in player_names}
+        self.max_streak = {name: 0 for name in player_names}
+        self.round_length = []
+
     def get_state(self):
-        pass
+        return {
+            "rounds_played": self.rounds_played,
+            "wins": self.wins,
+            "current_streak": self.current_streak,
+            "max_streak": self.max_streak,
+            "round_length": self.round_length,
+        }
+
+    def update_state(self, new_state):
+        self.rounds_played = new_state.get("rounds_played", self.rounds_played)
+        self.wins = new_state.get("wins", self.wins)
+        self.current_streak = new_state.get("current_streak", self.current_streak)
+        self.max_streak = new_state.get("max_streak", self.max_streak)
+        self.round_length = new_state.get("round_length", self.round_length)
+
+    def display_stats(self):
+        print(f"Rounds Played: {self.rounds_played}")
+        for player, wins in self.wins.items():
+            win_percentage = (wins / self.rounds_played) * 100
+            print(f"{player} won {wins} times ({win_percentage:.2f}%).")
+            print(f"{player}'s longest win streak: {self.max_streak[player]}")
+        print(
+            f"Average round length: {sum(self.round_length) / len(self.round_length)}"
+        )
 
 
-class HighCardGame(GameState):
-    def __init__(self, *players, io_interface: IOInterface):
+class HighCardGame:
+    def __init__(
+        self, *players, io_interface: IOInterface, game_state: HighCardGameState
+    ):
         self.players = players
         self.deck = Deck()
         self.io_interface = io_interface
-
-    def get_state(self):
-        """
-        Return a dictionary representing the current game state.
-        """
-        state = {
-            "players": [player.name for player in self.players],
-            "deck": str(self.deck),
-        }
-        return state
+        self.game_state = game_state
 
     async def play_round(self):
-        """
-        Plays a round of High Card.
-
-        Returns the player who drew the highest card.
-        """
+        start_time = time.time()
         self.deck.reset()
         high_card = None
         winner = None
-
-        # Randomize player order
-        player_list = list(self.players)  # convert tuple to list
+        player_list = list(self.players)
         random.shuffle(player_list)
 
         for player in player_list:
@@ -54,6 +71,42 @@ class HighCardGame(GameState):
                 high_card = drawn_card
                 winner = player
 
+        end_time = time.time()
+        round_length = end_time - start_time
+
+        if winner:
+            self.game_state.update_state(
+                {
+                    "rounds_played": self.game_state.rounds_played + 1,
+                    "wins": {
+                        **self.game_state.wins,
+                        winner.name: self.game_state.wins.get(winner.name, 0) + 1,
+                    },
+                    "current_streak": {
+                        **{
+                            name: 0
+                            for name in self.game_state.current_streak.keys()
+                            if name != winner.name
+                        },
+                        winner.name: self.game_state.current_streak.get(winner.name, 0)
+                        + 1,
+                    },
+                    "max_streak": {
+                        **{
+                            name: self.game_state.max_streak.get(name, 0)
+                            for name in self.game_state.max_streak.keys()
+                            if name != winner.name
+                        },
+                        winner.name: max(
+                            self.game_state.max_streak.get(winner.name, 0),
+                            self.game_state.current_streak.get(winner.name, 0) + 1,
+                        ),
+                    },
+                    "round_length": self.game_state.round_length + [round_length],
+                }
+            )
+        return winner
+
         return winner
 
 
@@ -63,7 +116,7 @@ def parse_args():
         "-r",
         "--rounds",
         type=int,
-        default=100,
+        default=1000000,
         help="number of rounds to play (default: 1000000)",
     )
     parser.add_argument(
@@ -83,21 +136,14 @@ async def main():
     args = parse_args()
 
     io_interface = DummyIOInterface()
-
     players = [SimplePlayer(name, io_interface) for name in args.names[: args.players]]
-    game = HighCardGame(*players, io_interface=io_interface)
+    game_state = HighCardGameState(args.names[: args.players])
+    game = HighCardGame(*players, io_interface=io_interface, game_state=game_state)
 
-    winners = []
     for _ in range(args.rounds):
-        winner = await game.play_round()
-        if winner is not None:
-            winners.append(winner.name)
+        await game.play_round()
 
-    print(f"Finished playing {args.rounds} rounds.")
-    for name in args.names[: args.players]:
-        wins = winners.count(name)
-        win_percentage = (wins / args.rounds) * 100
-        print(f"{name} won {wins} times ({win_percentage:.2f}%).")
+    game_state.display_stats()
 
 
 if __name__ == "__main__":
