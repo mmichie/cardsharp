54fd2b154b127e834c8260cdf43d677a560f0bc3
Matt Michie <mmichie@gmail.com>
2023-06-30 12:07:15 -0700

    Add Hand tests that excercise multiple cards, cards not in hand, ordering and empty hands

--- tests/test_hand.py
+++ tests/test_hand.py
@@ -34,3 +34,40 @@ def test_hand_str():
     card = Card(Suit.HEARTS, Rank.EIGHT)
     hand.add_card(card)
     assert str(hand) == "8 of ♥"
+
+
+def test_add_multiple_cards():
+    hand = Hand()
+    cards = [Card(Suit.HEARTS, Rank.EIGHT), Card(Suit.CLUBS, Rank.ACE)]
+    for card in cards:
+        hand.add_card(card)
+    assert all(card in hand.cards for card in cards)
+
+
+def test_remove_card_not_in_hand():
+    hand = Hand()
+    card = Card(Suit.HEARTS, Rank.EIGHT)
+    try:
+        hand.remove_card(card)
+    except ValueError:
+        pass  # Expected behavior
+    else:
+        assert False, "Expected ValueError when removing card not in hand"
+
+
+def test_order_of_cards():
+    hand = Hand()
+    cards = [Card(Suit.HEARTS, Rank.EIGHT), Card(Suit.CLUBS, Rank.ACE)]
+    for card in cards:
+        hand.add_card(card)
+    assert hand.cards == cards
+
+
+def test_empty_hand_repr():
+    hand = Hand()
+    assert repr(hand) == "Hand([])"
+
+
+def test_empty_hand_str():
+    hand = Hand()
+    assert str(hand) == ""
