14541ab24d79b13c7009f72865173c55e0b481b7
Matt Michie <mmichie@gmail.com>
2023-07-01 11:40:01 -0700

    Add typing and docstring to calculate_chi_square

--- cardsharp/common/util.py
+++ cardsharp/common/util.py
@@ -1,4 +1,18 @@
-def calculate_chi_square(observed_values, expected_values):
+from typing import List
+
+
+def calculate_chi_square(
+    observed_values: List[float], expected_values: List[float]
+) -> float:
+    """
+    Calculate the chi-square statistic given lists of observed and expected values.
+
+    :param observed_values: A list of observed values
+    :param expected_values: A list of expected values
+    :return: The calculated chi-square statistic
+    :raises ValueError: If the observed_values and expected_values lists do not have the same length
+
+    """
     if len(observed_values) != len(expected_values):
         raise ValueError("Observed and expected value lists must have the same length.")
 
