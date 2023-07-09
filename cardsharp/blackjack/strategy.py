6399f10a23e656d5be3d962f91df6110ad78f46a
Matt Michie <mmichie@gmail.com>
2023-07-09 15:49:34 -0700

    Refactor basic strategy class into clearer methods

--- cardsharp/blackjack/strategy.py
+++ cardsharp/blackjack/strategy.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
+from typing import Optional
+
 from cardsharp.blackjack.action import Action
-from cardsharp.common.card import Rank
+from cardsharp.common.card import Card, Rank
 
 
 class Strategy(ABC):
@@ -22,15 +24,46 @@ class DealerStrategy(Strategy):
 
 
 class BasicStrategy(Strategy):
-    def __init__(self):
-        pass
+    """
+    Basic blackjack strategy class that decides actions based on hand state and dealer's up card.
+    """
 
-    def decide_action(self, player, dealer_up_card) -> Action:
+    def decide_action(self, player, dealer_up_card: Card) -> Action:
+        """
+        Decides the action to take based on the player's hand and the dealer's up card.
+
+        Args:
+            player: The player instance.
+            dealer_up_card (Card): The dealer's up card.
+
+        Returns:
+            Action: The action to take.
+        """
         current_hand = player.current_hand
         if current_hand.is_blackjack():
             return Action.STAND
 
-        # Split
+        action = self._decide_on_split(current_hand, dealer_up_card)
+        if action is not None:
+            return action
+
+        action = self._decide_on_double(current_hand, dealer_up_card)
+        if action is not None:
+            return action
+
+        return self._decide_on_stand_or_hit(current_hand, dealer_up_card)
+
+    def _decide_on_split(self, current_hand, dealer_up_card: Card) -> Optional[Action]:
+        """
+        Decides whether to split based on the hand and the dealer's up card.
+
+        Args:
+            current_hand: The current hand of the player.
+            dealer_up_card (Card): The dealer's up card.
+
+        Returns:
+            Action: Action.SPLIT if the player decides to split, None otherwise.
+        """
         if current_hand.can_split():
             player_rank = current_hand.cards[0].rank
             dealer_rank = dealer_up_card.rank
@@ -51,9 +84,20 @@ class BasicStrategy(Strategy):
                 11,
             ]:
                 return Action.SPLIT
+        return None
 
+    def _decide_on_double(self, current_hand, dealer_up_card: Card) -> Optional[Action]:
+        """
+        Decides whether to double based on the hand and the dealer's up card.
+
+        Args:
+            current_hand: The current hand of the player.
+            dealer_up_card (Card): The dealer's up card.
+
+        Returns:
+            Action: Action.DOUBLE if the player decides to double, None otherwise.
+        """
         if current_hand.can_double():
-            # Double
             if (
                 current_hand.value() in [10, 11] and dealer_up_card.rank.rank_value < 10
             ) or (
@@ -62,7 +106,19 @@ class BasicStrategy(Strategy):
                 and dealer_up_card.rank.rank_value < 7
             ):
                 return Action.DOUBLE
+        return None
 
+    def _decide_on_stand_or_hit(self, current_hand, dealer_up_card: Card) -> Action:
+        """
+        Decides whether to stand or hit based on the hand and the dealer's up card.
+
+        Args:
+            current_hand: The current hand of the player.
+            dealer_up_card (Card): The dealer's up card.
+
+        Returns:
+            Action: Action.STAND or Action.HIT based on the player's hand and dealer's up card.
+        """
         if current_hand.value() <= 11:
             return Action.HIT
         elif current_hand.value() == 12:
